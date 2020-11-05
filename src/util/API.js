d9a0edc75cfeda9738665570d97316c4a9997838
webermg <matthew.gerald.weber@gmail.com>
2020-11-04 19:13:22 -0800

    created API module

---
+++ src/util/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=20&nat=us";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default {
+  search: function() {
+    return axios.get(BASEURL);
+  }
+};
