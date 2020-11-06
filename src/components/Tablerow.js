01c2a10194dcd6c95e8a7a2e142d12302336a7e8
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 19:24:23 -0800

    removed formatdate function

--- src/components/Tablerow.js
+++ src/components/Tablerow.js
@@ -1,19 +1,14 @@
 import React from 'react'
 
 export default function Tablerow(props) {
-  const formatDate = function(date) {
-    const temp = date.split(/T/)[0];
-    const temp2 = temp.split(/-/);
-    return `${temp2[1]}/${temp2[2]}/${temp2[0]}`
-  };
 
   return (
     <tr>
-      <th scope="row"><img alt="" src={props.pic}/></th>
-      <td>{props.first + " " + props.last}</td>
+      <th scope="row"><img alt="" src={props.image}/></th>
+      <td>{props.name}</td>
       <td>{props.phone}</td>
       <td>{props.email}</td>
-      <td>{formatDate(props.dob)}</td>
+      <td>{props.dob}</td>
     </tr>
   )
 }
