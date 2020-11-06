482d95872f66e37487aaf0397a0a97830f8a1b72
webermg <matthew.gerald.weber@gmail.com>
2020-11-06 07:22:25 -0800

    refactored for organization

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
     super();
     this.state = {
       data: [],
-      filtered: [],
+      filteredData: [],
       filter:"",
-      sorted:{
+      sortedState:{
         name:0,
         phone:0,
         email:0,
@@ -21,13 +21,11 @@ class App extends Component {
     }
   }
 
-  formatDate(date) {
-    const temp = date.split(/[-T]/);
-    return `${temp[1]}/${temp[2]}/${temp[0]}`
-  }
+  
 
+  //-----------lifecycle--------------
 
-  getPeople() {
+  componentDidMount() {
     API.search()
       .then(res => {
         let items = res.data.results.map(item => {
@@ -41,50 +39,70 @@ class App extends Component {
         })
         this.setState({ 
           data: items,
-          filtered:items
+          filteredData:items
         })
       })
       .catch(err => console.log(err));
   }
 
-  sortData = (field, asc, newSort) => {
-    let temp = this.state.filtered.map(item => {
-      return Object.assign({}, item)
-    });
-    temp.sort((item1, item2) => {
-      if (item1[field] > item2[field]) return asc;
-      else return -asc;
-    })
-    this.setState({ 
-      filtered: temp,
-      sorted:newSort
-    });
-  }
+  //-----------event handlers--------------
 
   onHeaderClick = (field) => {
-    let asc = this.state.sorted[field] <= 0 ? 1 : -1;
-    const newSort = {...this.state.sorted};
-    console.log(newSort)
-    for (const key in newSort) {
-      newSort[key] = 0;
-    }
-    newSort[field] = asc
-    this.sortData(field, asc, newSort);
-  }
-
-  componentDidMount() {
-    this.getPeople();
+    let dir = this.state.sortedState[field] <= 0 ? 1 : -1;
+    const newSortedState = this.getNewSortedState(field,dir);
+    const newData = this.getSortedData(field, dir);
+    const newFilteredData = this.state.filter !== "" ? this.getFilteredData(this.state.filter) : newData;
+    this.setState({
+      data:newData,
+      filteredData:newFilteredData,
+      sortedState:newSortedState
+    })
   }
 
   handleChange = (event) => {
-    
     const { name, value } = event.target;
-    const filteredData = this.state.data.filter(item => item.name.toLowerCase().includes(value.toLowerCase()))
+    const newFilteredData = this.getFilteredData(value)
     this.setState({
       [name]: value,
-      filtered:filteredData
+      filteredData:newFilteredData
     });
   }
+
+  //--------------utilities-----------------
+
+  //returns date in mm/dd/yyyy format
+  formatDate(date) {
+    const temp = date.split(/[-T]/);
+    return `${temp[1]}/${temp[2]}/${temp[0]}`
+  }
+
+  //returns sorted data array
+  getSortedData = (field, dir) => {
+    let copy = this.state.data.map(item => {
+      return Object.assign({}, item)
+    });
+    copy.sort((item1, item2) => {
+      if (item1[field] > item2[field]) return dir;
+      else return -dir;
+    })
+    return copy;
+  }
+
+  //returns array of sorted states
+  getNewSortedState = (field, dir) => {
+    const newSort = {...this.state.sortedState};
+    for (const key in newSort) {
+      newSort[key] = 0;
+    }
+    newSort[field] = dir
+    return newSort;
+  }
+
+  getFilteredData = filterStr => {
+    return this.state.data.filter(item => item.name.toLowerCase().includes(filterStr.toLowerCase()))
+  }
+
+  //-----------------render-----------
 
   render() {
     return (
@@ -92,7 +110,7 @@ class App extends Component {
         <Header />
         <div className="container">
           <Input value={this.state.filter} name="filter" type="text" title="search" onChange={this.handleChange} />
-          <Table status={this.state.sorted} data={this.state.filtered} onClick={this.onHeaderClick} />
+          <Table status={this.state.sortedState} data={this.state.filteredData} onClick={this.onHeaderClick} />
         </div>
       </div>
     );
